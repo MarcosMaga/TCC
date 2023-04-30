5694bfcf6392200fff1b2ea39c7acc689bf85996
Marcos Magalhães <marcosvmagalhaes123@gmail.com>
2023-04-30 00:34:04 -0300

    implementação do sistema de conexão ao Wi-Fi

--- Hardware/Firmware/smartwater/smartwater.ino
+++ Hardware/Firmware/smartwater/smartwater.ino
@@ -7,18 +7,20 @@ const char* ssid = "SmartWater";
 const char* password = "password";
 
 unsigned long previousMillis = 0;
+String macId = "";
 
 ESP8266WebServer server(80);
 
 void handleRoot() {
   // Crie a página web com o formulário HTML
+  WiFi.disconnect();
   String html = "<!DOCTYPE html>";
   html += "<html lang='pt'>";
   html += "<head><meta charset='UTF-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Config. Wifi</title></head>";
   html += "<body>";
-  html += "<h1 style='text-align: center; width: 40%;'>Configurar WiFi</h1>";
+  html += "<h1 style='text-align: center'>Configurar WiFi</h1>";
   html += "<form method='POST' action='/configurar-wifi'>";
-  html += "<label for='ssid'>Nome da rede WiFi:</label>";
+  html += "<label for='ssid'>Nome da rede WiFi:</label><br>";
   html += "<select id='ssid' name='ssid' required>";
 
   int networks = WiFi.scanNetworks();
@@ -27,7 +29,7 @@ void handleRoot() {
     html += "<option value='" + WiFi.SSID(i) + "'>" + WiFi.SSID(i) + "</option>";
   
   html += "</select><br><br>";
-  html += "<label for='senha'>Senha da rede WiFi:</label>";
+  html += "<label for='senha'>Senha da rede WiFi:</label><br>";
   html += "<input type='password' id='senha' name='senha' required><br><br>";
   html += "<input type='submit' value='Conectar'>";
   html += "</form></body></html>";
@@ -43,13 +45,36 @@ void handleConfigurarWiFi() {
 
   // Configure o ESP8266 para se conectar à rede WiFi
   WiFi.begin(ssid, senha);
-  while (WiFi.status() != WL_CONNECTED) {
+
+  int time = 0;
+  while (WiFi.status() != WL_CONNECTED && time < 6) {
     delay(1000);
+    time++;
+    Serial.println(time);
   }
 
-  // Redirecione o usuário para uma página de sucesso
-  server.sendHeader("Location", "http://192.168.4.1/sucesso");
-  server.send(302);
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.println("Não conectado!");
+    WiFi.disconnect();
+    server.sendHeader("Location", "http://192.168.4.1/falha");
+    server.send(302);
+    Serial.println("Redirecionado");
+  }else{
+    server.sendHeader("Location", "http://192.168.4.1/sucesso");
+    server.send(302);
+  }
+}
+
+void handleFalha(){
+  String html = "<!DOCTYPE html>";
+  html += "<html lang='pt'>";
+  html += "<head><meta charset='UTF-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Config. Wifi</title></head>";
+  html += "<body>";
+  html += "<h1>Senha incorreta!</h1>";
+  html += "<p>A senha colocada está incorreta.</p>";
+  html += "<a href='http://192.168.4.1/'>Voltar</a>";
+  html += "</body></html>";
+  server.send(200, "text/html", html);
 }
 
 void handleSucesso() {
@@ -59,12 +84,23 @@ void handleSucesso() {
   html += "<head><meta charset='UTF-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Config. Wifi</title></head>";
   html += "<body>";
   html += "<h1>Configuração concluída!</h1>";
-  html += "<p>O ESP8266 está agora conectado à rede WiFi.</p>";
+  html += "<p>O seu dispositivo agora está conectado à sua rede WiFi.</p>";
   html += "</body></html>";
   server.send(200, "text/html", html);
 }
 
+void startId(){
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
+  char macStr[18];
+  sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  macId = String(macStr);
+}
+
 void setup() {
+  Serial.begin(9600);
+  startId();
+  Serial.println(macId);
   pinMode(LED_BUILTIN, OUTPUT);
   // Inicie o modo de ponto de acesso do ESP8266
   WiFi.mode(WIFI_AP);
@@ -74,19 +110,21 @@ void setup() {
   server.on("/", handleRoot);
   server.on("/configurar-wifi", HTTP_POST, handleConfigurarWiFi);
   server.on("/sucesso", handleSucesso);
+  server.on("/falha", handleFalha);
   server.begin();
 }
 
 void loop() {
   server.handleClient();
+  
   if(WiFi.status() != WL_CONNECTED){
     unsigned long currentMillis = millis();
 
-    if (currentMillis - previousMillis >= 500) {
+    if(currentMillis - previousMillis >= 500){
       previousMillis = currentMillis;
       digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
     }
   }else{
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(LED_BUILTIN, HIGH);
   }
 }
