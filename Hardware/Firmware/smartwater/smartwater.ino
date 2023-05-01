3885323d598fe535e467713be0783f4be241269e
Marcos Magalhães <marcosvmagalhaes123@gmail.com>
2023-05-01 19:34:24 -0300

    Firebase adicionado no projeto

--- Hardware/Firmware/smartwater/smartwater.ino
+++ Hardware/Firmware/smartwater/smartwater.ino
@@ -2,6 +2,16 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+#include "secrets.h"
+
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+bool signupOK = false;
+
 // Defina o nome e a senha do ponto de acesso do ESP8266
 const char* ssid = "SmartWater";
 const char* password = "password";
@@ -25,9 +35,9 @@ void handleRoot() {
 
   int networks = WiFi.scanNetworks();
 
-  for(int i=0; i < networks; i++)
+  for (int i = 0; i < networks; i++)
     html += "<option value='" + WiFi.SSID(i) + "'>" + WiFi.SSID(i) + "</option>";
-  
+
   html += "</select><br><br>";
   html += "<label for='senha'>Senha da rede WiFi:</label><br>";
   html += "<input type='password' id='senha' name='senha' required><br><br>";
@@ -53,19 +63,19 @@ void handleConfigurarWiFi() {
     Serial.println(time);
   }
 
-  if(WiFi.status() != WL_CONNECTED){
+  if (WiFi.status() != WL_CONNECTED) {
     Serial.println("Não conectado!");
     WiFi.disconnect();
     server.sendHeader("Location", "http://192.168.4.1/falha");
     server.send(302);
     Serial.println("Redirecionado");
-  }else{
+  } else {
     server.sendHeader("Location", "http://192.168.4.1/sucesso");
     server.send(302);
   }
 }
 
-void handleFalha(){
+void handleFalha() {
   String html = "<!DOCTYPE html>";
   html += "<html lang='pt'>";
   html += "<head><meta charset='UTF-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Config. Wifi</title></head>";
@@ -87,9 +97,23 @@ void handleSucesso() {
   html += "<p>O seu dispositivo agora está conectado à sua rede WiFi.</p>";
   html += "</body></html>";
   server.send(200, "text/html", html);
+
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+
+  if (Firebase.signUp(&config, &auth, "", "")) {
+    Serial.println("Login Firebase Ok");
+    signupOK = true;
+  } else
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+
+  config.token_status_callback = tokenStatusCallback;
+
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 }
 
-void startId(){
+void startId() {
   uint8_t mac[6];
   WiFi.macAddress(mac);
   char macStr[18];
@@ -116,15 +140,30 @@ void setup() {
 
 void loop() {
   server.handleClient();
-  
-  if(WiFi.status() != WL_CONNECTED){
+
+  if (WiFi.status() != WL_CONNECTED) {
     unsigned long currentMillis = millis();
 
-    if(currentMillis - previousMillis >= 500){
+    if (currentMillis - previousMillis >= 500) {
       previousMillis = currentMillis;
       digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
     }
-  }else{
+  } else {
     digitalWrite(LED_BUILTIN, HIGH);
+    unsigned long currentMillis = millis();
+
+    if (currentMillis - previousMillis >= 1000) {
+      if (Firebase.ready() && signupOK) {
+        if (Firebase.RTDB.setString(&fbdo, "devices/" + macId, random(101))) {
+          Serial.println("PASSED");
+          Serial.println("PATH: " + fbdo.dataPath());
+          Serial.println("TYPE: " + fbdo.dataType());
+        } else {
+          Serial.println("FAILED");
+          Serial.println("REASON: " + fbdo.errorReason());
+        }
+      }
+      previousMillis = currentMillis;
+    }
   }
 }
